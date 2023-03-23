35f20eafab112b9063d2de22cf2d69e7e6a16e7a
supredu <1020665094@qq.com>
2023-03-23 14:48:29 +0800

    modify

--- hardhatKun/test/test.js
+++ hardhatKun/test/test.js
@@ -1,12 +1,18 @@
 describe("NFTMarkettest", function() {
     it("Should list another sales", async function() {
       console.log("start")
-      const nftMarketplace = 0x26B2DC74Aa745bd61598290C00c90e49831ADc41
+      const [_] = await ethers.getSigners()
+      const address = '0x26B2DC74Aa745bd61598290C00c90e49831ADc41'
+      const abiNFT = [
+        "function getListingPrice() public view returns (uint256)",
+        "function listToken(uint256 tokenId, uint256 price) public payable returns (bool)"
+    ];
+
+      const nftMarketplace = new ethers.Contract(address, abiNFT, _)
 
   
       let listingPrice = await nftMarketplace.getListingPrice()
       listingPrice = listingPrice.toString()
-      const lockPrice = ethers.utils.parseUnits('100000', 'ether')
       const auctionPrice = ethers.utils.parseUnits('1', 'ether')
       await nftMarketplace.listToken(2, auctionPrice, { value: listingPrice })
       console.log("list token2 success")
