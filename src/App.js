3b22e0493fe2eb6ae2420ccb013045ac4b3ccfdc
xiyang <jbossbc@gmail.com>
2023-03-25 17:18:58 +0800

    connect wallet

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 import "./App.css";
-
+import MyHead from "./components/myHead";
+import Web3 from "web3";
+import { Web3ReactProvider } from '@web3-react/core'
 function App() {
-    const logoSpanStyle=sty
-  return (
-    <div className="container flex flex-col" >
-        <div className="head">
-            <div height="100%" className="headCenter">
-                <nav className= "Navbar--main">
-                    <div className="logobox">
-                        <a className="logo">
-                            <div height="40" width="40" className="icon">
-                                <span style={{box-sizing:"border-box",display:block,overflow:hidden,width:initial,height:initial;background:none;opacity:1;border:0;margin:0;padding:0;position:absolute;top:0;left:0;bottom:0;right:0}}>
-                                    <img alt="Kun Logo" src="./logo.png" decoding="async" data-nimg="fill" style={{position:"absolute",top:0,left:0,bottom:0,right:0,box-sizing:border-box,padding:0,border:none,margin:auto,display:block,width:0,height:0,min-width:100%,max-width:100%,min-height:100%,max-height:100%}}/>
-                                </span>
-                            </div>
-                            <div data-testid="brand-name" className="marketname">
-                                <svg fill="white" xmlns="http://www.w3.org/2000/svg" height="58" style="width:100px" viewBox="0 0 313 58" width="313" >
-                                    <g id="Layer_1">
-                                        <title>Layer 1</title>
-                                        <text font-weight="bold" font-style="italic" transform="matrix(6.27469 0 0 2.01378 -551.81 -17.6611)" stroke="#ffffff"  text-anchor="start" font-family="'Rubik'" font-size="24" id="svg_2" y="33.61225" x="86.92808" stroke-width="0" fill="#000000">KUN</text>
-                                    </g>
-                                </svg>
-                            </div>
-                        </a>
-                    </div>
-                    <ul className="">
+    const NFTs=0;
+    function getLibrary(){
 
-                    </ul>
-                </nav>
-            </div>
-        </div>
+    }
+  return (
+      <div className="body">
+    <div style={{height:"72",minHeight:"72",maxHeight:"72"}}>
+      <MyHead style={{height:"72",minHeight:"72",maxHeight:"72"}}/>
     </div>
+          <div className="content">
+          <div>
+              搜索
+          </div>
+          <div>
+
+          </div>
+          <div>
+              <span>NFT1</span>
+          </div>
+              <div>
+                  <span>NFT2</span>
+              </div>
+              <div>
+                  <span>NFT1</span>
+              </div>
+              <div>
+                  <span>NFT2</span>
+              </div>
+          </div>
+      </div>
   );
 }
 
--- src/components/myHead.js
+++ src/components/myHead.js
@@ -1,12 +1,13 @@
 import {ShoppingCartOutlined,WalletOutlined,UserOutlined} from "@ant-design/icons";
-import {Menu} from 'antd';
 import logo from "../logo.png";
 import font from "../font.svg";
 import "./myHead.css";
 import {useState} from "react";
-import "./wallet";
-import walletSupport from "./wallet";
+import metamask from "../metamask.png";
+import {Modal} from "antd";
+
 function MyHead(){
+    const {ethereum}=window;
     const [walletState,setWalletState]=useState(false)
     function loginWallet(){
         setWalletState(true)
@@ -14,9 +15,35 @@ function MyHead(){
     function userInfo(){
       alert("welcome to userInfo");
     }
-    function shoppingCart(){
-    alert("welcome to shoppingCart");
+    function hideWalletModal(){
+        setWalletState(false);
     }
+    function shoppingCart(){
+    alert("此功能待开放");
+    }
+    const [currentAccount, setCurrentAccount] = useState(null);
+
+    async function connect(){
+        if(!ethereum){
+            alert("Make sure you have metamask installed");
+            return;
+        }
+        try{
+            const accounts=await ethereum.request({method:'eth_requestAccounts'});
+            setCurrentAccount(accounts[0]);
+        }catch(err){
+            alert("connect failed");
+        }
+    }
+
+    // async function disconnect(){
+    //     try{
+    //         deactivate();
+    //     }catch(ex){
+    //         alert("断开连接失败");
+    //     }
+    // }
+
     return (
         <div className="header">
              <div className="logo">
@@ -36,7 +63,6 @@ function MyHead(){
                 <div className="iconitem">
                     <div>
                     <WalletOutlined onClick={loginWallet} style={{fontSize:"32px",color:"white"}} />
-                        
                     </div>
                 </div>
                 <div  className="iconitem">
@@ -45,6 +71,29 @@ function MyHead(){
                     </div>
                 </div>
         </div>
+            <Modal title="My Wallet" open={walletState} onCancel={hideWalletModal} cancelText="取消">
+                <div>
+                    <span>
+                        If you don't have a wallet yet, you can select a provider and create one now.
+                    </span>
+                </div>
+                <div>
+                    <ul>
+                        <li className="walletsli">
+                            <button className="wallets" onClick={connect}>
+                                <div className="img">
+                                    <img height="30" src={metamask} />
+                                </div>
+                                <div className="font">
+                                    <span>
+                                        MetaMask
+                                    </span>
+                                </div>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </Modal>
         </div>
     )
 }
